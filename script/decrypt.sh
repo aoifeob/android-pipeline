ace0cb29a25b12911b76938b83ca59d4d059d527
Aoife <23223426+aoifeob@users.noreply.github.com>
2024-05-24 02:28:03 +0100

    test

--- script/decrypt.sh
+++ script/decrypt.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-gpg --pinentry-mode loopback --quiet --batch --yes --decrypt --passphrase="$GOOGLE_SERVICES_KEY" \
+gpg2 --quiet --batch --yes --decrypt --passphrase="$GOOGLE_SERVICES_KEY" \
 --output app/google-services.json app/google-services.json.gpg
 
-gpg --pinentry-mode loopback --quiet --batch --yes --decrypt --passphrase="$FIREBASE_SERVICE_ACC" \
+gpg2 --quiet --batch --yes --decrypt --passphrase="$FIREBASE_SERVICE_ACC" \
 --output app/firebase-service-account.json app/firebase-service-account.json.gpg
