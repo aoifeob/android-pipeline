3dc46d364cdc01b6091bc1f4ca30512b0b4300e3
Aoife O' Brien <75118463+obrienao@users.noreply.github.com>
2024-05-24 02:21:16 +0100

    Add files via upload

---
+++ decrypt.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+gpg --quiet --batch --yes --decrypt --passphrase="$GOOGLE_SERVICES_KEY" \
+--output ../app/google-services.json ../app/google-services.json.gpg
+
+gpg --quiet --batch --yes --decrypt --passphrase="$FIREBASE_SERVICE_ACC" \
+--output ../app/firebase-service-account.json ../app/firebase-service-account.json.gpg
