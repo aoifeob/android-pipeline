92e25e2ee90a283080dbf65f3a2d3f89c57ebe84
Aoife O' Brien <75118463+obrienao@users.noreply.github.com>
2024-05-24 02:21:05 +0100

    Delete script/decrypt.sh

--- script/decrypt.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-gpg --quiet --batch --yes --decrypt --passphrase="$GOOGLE_SERVICES_KEY" \
---output ../app/google-services.json ../app/google-services.json.gpg
-
-gpg --quiet --batch --yes --decrypt --passphrase="$FIREBASE_SERVICE_ACC" \
---output ../app/firebase-service-account.json ../app/firebase-service-account.json.gpg
